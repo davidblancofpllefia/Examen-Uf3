48dedd53eb350337e4b8d3a05b44e906b8955950
daw2-blanco24 <blancofernandezdavid@fpllefia.com>
2024-03-08 18:03:45 +0100

    Desplegamiento

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import { header } from "./componentes/header"
-import { pedidos } from "./componentes/pedidos"
-import { tablaPedidos } from "./componentes/tablaPedidos"
+import { header } from "./src/componentes/header"
+import { pedidos } from "./src/componentes/pedidos"
+import { tablaPedidos } from "./src/componentes/tablaPedidos"
 
 document.querySelector('header').innerHTML = header.template
 document.querySelector('main').innerHTML = pedidos.template
